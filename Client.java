c4ffd5830ac035a95b4a1a9ce86887d8cab3a038
prathihasreddy88@gmail.com <prathihasreddy88@gmail.com>
2023-04-19 18:03:44 -0400

    second commit

---
+++ Client.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.net.*;
+
+
+class Client {
+
+	public static void main(String args[])
+		throws Exception
+	{
+		BufferedReader keyboard
+			= new BufferedReader(
+				new InputStreamReader(System.in));
+
+		String portCommand=keyboard.readLine();
+		String str2[]=portCommand.split(" ");
+		int port = Integer.parseInt(str2[1]);
+
+		try
+		{
+			Socket s = new Socket("localhost", port);
+            //System.out.println("connected");
+			DataOutputStream dos
+				= new DataOutputStream(
+					s.getOutputStream());
+
+			DataInputStream din=new DataInputStream(s.getInputStream());
+
+			while(true)
+			{
+                //System.out.println("entered loop");
+				String fileCommand=keyboard.readLine();
+				str2=fileCommand.split(" ");
+                if(str2[0].equals("exit"))
+                    break;
+				if (str2[0].equals("get")) 
+				{
+                    //System.out.println("entered get");
+					dos.writeUTF("1");
+					String fileName = str2[1].substring(0,str2[1].length());
+					dos.writeUTF(fileName);
+					if(din.readUTF().equals("1"))
+					{
+						byte buffer[] = new byte[1000];
+						String path= "C:\\Users\\q\\Downloads\\CN_Project2_files\\";
+						File myFile = new File(path+"new"+fileName);
+						FileOutputStream fout = new FileOutputStream(myFile);
+						int valid;
+						while((valid = din.read(buffer))>-1)
+						{
+							fout.write(buffer,0,valid);
+							if(valid<1000)
+								break;
+						}
+						fout.close();
+					}
+					else
+					{
+						System.out.println("file does no exist : enter correct filename");
+					}
+				}
+				else
+				{
+					if(str2[0].equals("upload"))
+					{
+                        //System.out.println("entered upload");
+                        //System.out.println("after");
+						String fileName = str2[1].substring(0,str2[1].length());
+						File file= new File("C:\\Users\\q\\Downloads\\CN_Project2_files\\"+fileName);
+						if(file.exists())
+						{
+                            //System.out.println("entering data to file");
+							dos.writeUTF("0");
+                        	//System.out.println("sent data to server");
+                        	//System.out.println("recieved data from server"+fileName);
+							dos.writeUTF(fileName);
+							FileInputStream fin= new FileInputStream(file);
+							int ch;
+							byte arr[]=new byte[1000];
+							while((ch=fin.read(arr))>-1)
+							{
+								dos.write(arr,0,ch);
+							}
+                            //System.out.println("completed entering data into file");
+							fin.close();
+                            //System.out.println("end of upload");
+						}
+						else
+						{
+							System.out.println("file does not exist : enter correct filename");
+						}
+					}
+					else
+					{
+						System.out.println("Entered wrng command : enter correct command");
+					}
+                    //System.out.println("end of loop");
+				}
+				dos.flush();
+			}
+			dos.close();
+			keyboard.close();
+			s.close();
+		}
+		catch(Exception e)
+		{
+            System.out.println(""+e);
+			//System.out.println("entered wrong port number: please restart and enter correct port number");
+		}
+	}
+}
--- Server.java
+++ Server.java
@@ -1,1 +1,95 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
 
+class example extends Thread
+{
+    Socket s;
+    DataInputStream din;
+    DataOutputStream ps;
+    public example(Socket st)
+    {
+        s=st;
+    }
+    public void run()
+    {
+        try
+        {
+                //System.out.println("entered");
+            din=new DataInputStream(s.getInputStream());
+            ps=new DataOutputStream(s.getOutputStream());
+            while(s.isConnected())
+            {
+                if(din.readUTF().equals("1"))
+                        {
+                            String fileName = din.readUTF();
+                            //System.out.println(""+fileName);
+                            File file= new File("C:\\Users\\q\\Downloads\\CN_Project2_files\\"+fileName);
+                            if(file.exists())
+                            {
+                                //System.out.println("exists");
+                                ps.writeUTF("1");
+
+                                FileInputStream fin= new FileInputStream(file);
+
+                                int ch=-1;
+                                byte arr[]=new byte[1000];
+                                while((ch=fin.read(arr))>-1)
+                                {
+                                    ps.write(arr,0,ch);
+                                }
+                                //System.out.println("wrote file");
+                                fin.close();
+                            }
+                            else
+                            {
+                                ps.writeUTF("0");
+                            }
+                            //ps.flush();
+                        }
+                        else
+                        {
+                            String fileName = din.readUTF();
+                            //System.out.println("got filename to upload data in server"+" "+fileName);
+                            byte buffer[] = new byte[1000];
+                            String path= "C:\\Users\\q\\Downloads\\CN_Project2_files\\";
+                            //System.out.println("creating new File");
+                            File myFile = new File(path+"new"+fileName);
+
+                            FileOutputStream fout = new FileOutputStream(myFile);
+                            int valid;
+                            while((valid = din.read(buffer))>-1)
+                            {
+                                fout.write(buffer,0,valid);
+                                if(valid<1000)
+                                    break;
+                            }
+                            fout.close();
+                        }
+                //ps.flush();
+            }
+            ps.close();
+            din.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("client disconnected");
+        }
+    }
+}
+class Server {
+
+	public static void main(String args[])
+		throws Exception
+	{
+            ServerSocket ss = new ServerSocket(5106);
+            while(true)
+            {
+                Socket s = ss.accept();
+                //System.out.println("connected");
+                example Thread1 = new example(s);
+                //System.out.println("thread started");
+                Thread1.start();
+            }
+	}
+}
